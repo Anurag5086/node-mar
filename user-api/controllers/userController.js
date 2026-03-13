4d9e21b5c5f53652b5256d8f0564d8cb1465d5d8
Anurag5086 <anuraggupta5086@gmail.com>
2026-03-13 20:36:31 +0530

    user api done

--- user-api/controllers/userController.js
+++ user-api/controllers/userController.js
@@ -13,3 +13,41 @@ exports.createUser = async (req, res) => {
 
     res.status(201).json({ message: 'User created successfully', user: newUser });
 };
+
+exports.getUserById = async (req, res) => {
+    const userId = req.params.id;
+
+    const user = await User.findById(userId);
+
+    if(!user){
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ user });
+}
+
+exports.updateUser = async (req, res) => {
+    const userId = req.params.id;
+
+    const { name, email, age } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {name, email, age}, { new: true });
+
+    if(!updatedUser){
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+}
+
+exports.deleteUser = async (req, res) => {
+    const userId = req.params.id;
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if(!deletedUser){
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ message: 'User deleted successfully' });
+}
--- user-api/routes/userRoutes.js
+++ user-api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController');
+const { createUser, getUserById, updateUser, deleteUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/user', createUser);
